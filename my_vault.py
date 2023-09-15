563c98ef1ffd9b23fbba505cce236e24b99005c7
Chris Jessup <chris.jessup@hellofresh.com.au>
2023-09-15 14:49:49 +1000

    Added path resolution & refactored into functions

--- my_vault.py
+++ my_vault.py
@@ -1,23 +1,52 @@
 import os
+from typing import Dict, Union
 import requests
 import sys
 
+#
 # Get env from server
-req = requests.get('http://localhost:8000/env.json').json()
+#
+def get_variables_from_vault_server() -> Dict[str, str]:
+    return requests.get('http://localhost:8000/env.json').json()
 
+#
 # Search through env replacing the appropriate items
-for key, value in os.environ.items():
-    if value.startswith('vault:'):
-        vault_key = value.replace('vault:', '')
-        if vault_key in req:
-            print(f">  Setting env[{key}]={req[vault_key]}", file=sys.stderr)
-            os.environ[key] = req[vault_key]
+#
+def replace_environment(req: Dict[str, str]) -> None:
+    for key, value in os.environ.items():
+        if value.startswith('vault:'):
+            vault_key = value.replace('vault:', '')
+            if vault_key in req:
+                print(f">  Setting env[{key}]={req[vault_key]}", file=sys.stderr)
+                os.environ[key] = req[vault_key]
 
+#
 # Run our program
-prog = sys.argv[1]
-print(">  Going to run '{prog}' with the new env", file=sys.stderr)
+#
+def resolve_fully_qualified_path_of_program(program_name: str) -> Union[str, None]:
+    if not program_name.startswith("/"):
+        #
+        # Search the path for a relevant executable
+        #
+        for directory in os.environ.get("PATH","").split(":"):
+            possible_program = f"{directory}/{program_name}"
+            if os.path.exists(possible_program):
+                return possible_program
+        
 
-# Note we are not using PATH here so you need to point to the
-# fully qualified path of the program eg '/bin/bash' rather
-# than just 'bash'
-os.execl(prog, prog) # Kind of more obvious now we are in bash
+
+if __name__ == '__main__':
+
+    variables = get_variables_from_vault_server()
+    replace_environment(variables)
+    program_name = sys.argv[1]
+    program = resolve_fully_qualified_path_of_program(program_name)
+    if not program:
+        print(f"Couldn't resolve executable for {program_name}", file=sys.stderr)
+        sys.exit()
+        
+    print(">  Going to run '{program_name}' with the new env", file=sys.stderr)
+
+    # Execute the program, ie replace this program with the one specified
+    os.execl(program, program_name) # Kind of more obvious now we are in bash
+    print("You will never get here", file=sys.stderr)
